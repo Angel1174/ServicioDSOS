603f732018c36c7c801af0bd3f7193ad47e06393
Angel1174 <43913819+Angel1174@users.noreply.github.com>
2022-06-10 20:32:35 -0500

    Update App.js
    Anexando las rutas de las vistas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import React from 'react';
 import Principal from './components/Principal';
 import Login from './components/Login';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <Principal></Principal>
+    <Router>
+            <Routes>
+              <Route exact path="/" element={<Login/>}>
+              </Route>
+              <Route exact path="/usuarios" element={<Principal/>}>
+              </Route>
+            </Routes>
+    </Router>
   );
 }
 
