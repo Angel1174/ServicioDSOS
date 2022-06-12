d8bd67e910312df2ff7b499294eafc6f23a649cb
Angel1174 <43913819+Angel1174@users.noreply.github.com>
2022-06-12 13:32:40 -0500

    Anexando todas las rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import './App.css';
+import './css/App.css';
 import React from 'react';
-import Principal from './components/Principal';
-import Login from './components/Login';
+import Principal from './views/Principal';
+import Login from './views/Login';
+import Validar from './views/Validar';
+import Recuperar from './views/Recuperar';
+import { EnviarEmail } from './views/EnviarEmail';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
+
+
 
 function App() {
   return (
     <Router>
             <Routes>
+            <Route exact path="/usuarios" element={<Principal/>}>
+              </Route>
+              <Route exact path="/validar" element={<Validar/>}>
+              </Route>
+              <Route exact path="/recuperar" element={<Recuperar/>}>
+              </Route>
+              <Route exact path="/registrar" element={<EnviarEmail/>}>
+              </Route>
               <Route exact path="/" element={<Login/>}>
               </Route>
-              <Route exact path="/usuarios" element={<Principal/>}>
-              </Route>
+              
             </Routes>
     </Router>
   );
 }
 
 export default App;
+
